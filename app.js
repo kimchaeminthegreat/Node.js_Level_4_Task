f8d0893c77c0262a0c4fd73459e2ab70f1a2a011
KIM, CHAE-MIN <gekcm@naver.com>
2023-05-04 17:38:56 +0900

    It's second commit for appplied Swagger and added readme for Swagger Link and  Diagram Link

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const authRouter = require("./routes/auth.route");
 const postsRouter = require("./routes/posts.route");
 const commentsRouter = require("./routes/comments.route");
 const likesRouter = require("./routes/likes.route");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output");
 
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", [authRouter]);
 app.use("/posts", [likesRouter, postsRouter, commentsRouter]);
+
 
 app.listen(port, () => {
   console.log(port, "Port Has Opened!!!");
